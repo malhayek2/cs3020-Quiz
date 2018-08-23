28c4c8cf178acf4616e0ac62218d59ac33d80352
Mohanad Alhayek <malhayek@CIT-SCC113-1137.cs.dixie.edu>
2018-08-23 09:59:29 -0600

    day2 button functions

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // model stuff below
+    let questions : [String] = [
+    "What is 9 + 10 ?",
+    "What comes after Sunday?",
+    "What comes after Red Green ...?"
+    
+    ]
+    let answer : [String] = [
+        "19",
+        "Monday",
+        "blue"
+    ]
+    // controller stuff below
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        self.questionLabel.text = "What Color is the sky?"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+    }
 }
 
